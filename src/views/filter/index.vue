27c650f815759afe0fc009649d455a40c34fc5fa
xiaoli <1480809371@qq.com>
2023-02-13 18:39:35 +0800

    fix: filter

--- src/views/filter/index.vue
+++ src/views/filter/index.vue
@@ -2,7 +2,10 @@
   <div>
     filter
     <hr>
-    {{ message | capitalize(1111) }}
+    {{ message | capitalize }}
+    <hr>
+    {{ message | capi }}
+    <hr>
     <ul>
       <li v-for="item in crr" :key="item">{{ item }}</li>
     </ul>
@@ -13,22 +16,30 @@
 <script>
 export default {
   name: 'Index-Filter',
-  filters: {
-    capitalize: function (value) {
-      if (!value) return ''
-      value = value.toString()
-      return value.charAt(0).toUpperCase() + value.slice(1)
-    }
-  },
-
   data() {
     return {
-      message: '',
+      message: 'hello vue.js!',
       arr: ['1', '2', '3', '4', '5', '6',],
       brr: ['7', '8', '9', '10'],
       crr: [],
       num: 1
     };
+  },
+  filters: {
+    // 过滤器函数接收表达式的值（之前的操作链的结果）作为第一个参数
+    // message的值 作为参数 传入到函数中
+    capitalize: function (value) {
+      console.log(value, 'value')
+      if (!value) return ''
+      value = value.toString()
+      return value.charAt(0).toUpperCase() + value.slice(1)
+    },
+    capi(val) {
+      console.log(val)
+      const first = val.charAt(0).toUpperCase();
+      const other = val.slice(1);
+      return first + other + ' 调用了局部过滤器';
+    }
   },
 
   mounted() {
@@ -42,7 +53,6 @@ export default {
       if (this.num === 2) {
         this.crr = this.arr.concat(this.brr)
       } else {
-        console.log(111)
         this.crr = this.arr.concat(this.brr)
       }
     }
