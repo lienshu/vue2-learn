af9e31db25cebe01513dd5ac6c8eed045a18a7b7
xiaoli <1480809371@qq.com>
2023-05-19 16:02:53 +0800

    fix: echarts示例

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ const {
 } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  lintOnSave: false
+  lintOnSave: false,
   pluginOptions: {
     windicss: {
       // 具体配置请查看 https://github.com/windicss/vite-plugin-windicss/blob/main/packages/plugin-utils/src/options.ts
